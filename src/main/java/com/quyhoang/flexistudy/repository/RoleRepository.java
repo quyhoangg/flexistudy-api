a449481a03b3b9b344f04390ad07757a8bee9311
Quy Hoang <mausuper202@gmail.com>
2025-10-17 17:33:03 +0700

    fix role, role permission

--- src/main/java/com/quyhoang/flexistudy/repository/RoleRepository.java
+++ src/main/java/com/quyhoang/flexistudy/repository/RoleRepository.java
@@ -5,10 +5,13 @@ import com.quyhoang.flexistudy.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, RoleName> {
+public interface RoleRepository extends JpaRepository<Role, String> {
     Optional<Role> findByName(RoleName name);
+    List<Role> findByNameIn(List<RoleName> names);
+    void deleteByName(RoleName name);
 }
 
