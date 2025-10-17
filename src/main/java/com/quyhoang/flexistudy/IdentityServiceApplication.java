ce35dcccb241a394538afc2e994367989628974e
Quy Hoang <mausuper202@gmail.com>
2025-10-17 21:06:31 +0700

    .env pom

--- src/main/java/com/quyhoang/flexistudy/IdentityServiceApplication.java
+++ src/main/java/com/quyhoang/flexistudy/IdentityServiceApplication.java
@@ -9,9 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class IdentityServiceApplication {
     public static void main(String[] args) {
-        // ✅ Load .env trước khi Spring khởi động
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(IdentityServiceApplication.class, args);
     }
 }
