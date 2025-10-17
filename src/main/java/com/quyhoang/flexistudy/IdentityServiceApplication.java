7a2e0203bb9ea7835aeade047c36b29b5e488013
Quy Hoang <mausuper202@gmail.com>
2025-10-17 21:12:32 +0700

    fix error

--- src/main/java/com/quyhoang/flexistudy/IdentityServiceApplication.java
+++ src/main/java/com/quyhoang/flexistudy/IdentityServiceApplication.java
@@ -1,6 +1,5 @@
 package com.quyhoang.flexistudy;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
