1c6c7c611369f39107b21962f63bcda2244a0973
Quy Hoang <mausuper202@gmail.com>
2025-10-17 20:50:43 +0700

    add .env file

--- src/main/java/com/quyhoang/flexistudy/IdentityServiceApplication.java
+++ src/main/java/com/quyhoang/flexistudy/IdentityServiceApplication.java
@@ -1,5 +1,6 @@
 package com.quyhoang.flexistudy;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class IdentityServiceApplication {
     public static void main(String[] args) {
+        // ✅ Load .env trước khi Spring khởi động
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(IdentityServiceApplication.class, args);
     }
 }
